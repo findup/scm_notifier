13b30b5e8a968606332dbafe8f99a922df3bb745
toshiyuki_sugimoto <toshiyukisu2276@ptc.zenrin.co.jp>
2017-02-03 16:45:31 +0900

    Use mysql2 on Sequel.

--- ruby/www/main.rb
+++ ruby/www/main.rb
@@ -1,18 +1,20 @@
 require 'rubygems'
 #require 'bundler'
 require 'sequel'
+#require 'sqlite3'
 require 'sinatra'
 require 'sinatra/reloader'
 
 #Bundler.require(:default)
 
 #DB = Sequel.connect('sqlite://test.db')
+DB = Sequel.connect('mysql2://mysql_user:mysql_pw@mysql/mysql_database')
 
-# DB.create_table :items do
-#  primary_key :id
-#  String :name
-#  Integer :price
-#end
+DB.create_table :items do
+  primary_key :id
+  String :name
+  Integer :price
+end
 
 set :bind, '0.0.0.0' # webrick for remote host.
 
