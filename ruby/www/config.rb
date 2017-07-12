e6c308af92f7cc17a854dcd40603539759ffa55f
toshiyuki_sugimoto <findup01@gmail.com>
2017-07-12 17:22:06 +0900

    add backtrace.

--- ruby/www/config.rb
+++ ruby/www/config.rb
@@ -3,6 +3,7 @@ require 'yaml'
 # 設定情報読み出し
 def read_config
     yaml = YAML.load_file("config.yml")
+
     config_hash = {}
     config_hash[:url] = yaml["repo_url"]
     config_hash[:username] = yaml["username"]
@@ -10,5 +11,7 @@ def read_config
     config_hash[:interval] = yaml["interval"]
     config_hash[:proxy_host] = yaml["proxy_host"]
     config_hash[:proxy_port] = yaml["proxy_port"]
+    config_hash[:backtrace] = yaml["backtrace"]
+
     return config_hash
 end
